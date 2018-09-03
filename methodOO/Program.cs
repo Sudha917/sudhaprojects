73f546f3caac62b1fa809d8915ac1bd287a0341e
sudha917 <sudha.avinash917.com>
2018-09-03 20:13:45 +0530

    Mthd Overloading

---
+++ methodOO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodOO
+{
+    class Father
+    {
+        public virtual void Address()
+        {
+            Console.WriteLine("Father Address");
+        }
+    }
+    class Son:Father
+    {
+        public new void Address()       // use new keyword to hide the mtd
+        {
+            Console.WriteLine("Son Address");
+        }
+    }
+    class Mother:Father
+    {
+        public override void Address()
+        {
+            Console.WriteLine("Mother Address");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Father fobj = new Father();
+            fobj.Address();
+
+            fobj = new Son();
+            fobj.Address();     // mtd hiding
+
+            fobj = new Mother();
+            fobj.Address();         // calling overriding mtd
+
+            Mthdverload mo = new Mthdverload();   //mtd overloading
+            int x= mo.Add(10, 20);
+            double y = mo.Add(10.7, 20.12);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodOO/mthdverload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodOO
+{
+    class Mthdverload
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+  
+        public double Add(double x, double y)
+            {
+                return x+y;
+            }
+    }
+
+}
